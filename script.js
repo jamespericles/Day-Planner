a68530ae4747fe311b8a9be8e32d4c38ed6e8fd8
jamespericles <63256479+jamespericles@users.noreply.github.com>
2020-06-12 22:53:38 -0400

    updated button functionality

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
-const saveBtns = document.querySelectorAll(".saveBtn");
+$(document).ready(function () {
+  function updateDOM() {
+    let savedEvent9am = localStorage.getItem("9am");
+    let display9am = $("#display9am");
+    display9am.html(savedEvent9am);
 
-for (let i = 0; i < saveBtns.length; i++) {
-  saveBtns[i].addEventListener("click", function (e) {
-    // console.log(e.target.id);
-    let id = e.target.id;
-    console.log(saveBtns[i].parent);
-  });
-}
+    let savedEvent10am = localStorage.getItem("10am");
+    let display10am = $("#display10am");
+    display10am.html(savedEvent10am);
+
+    let savedEvent11am = localStorage.getItem("11am");
+    let display11am = $("#display11am");
+    display11am.html(savedEvent11am);
+
+    let savedEvent12pm = localStorage.getItem("12pm");
+    let display12pm = $("#display12pm");
+    display12pm.html(savedEvent12pm);
+
+    let savedEvent1pm = localStorage.getItem("1pm");
+    let display1pm = $("#display1pm");
+    display1pm.html(savedEvent1pm);
+
+    let savedEvent2pm = localStorage.getItem("2pm");
+    let display2pm = $("#display2pm");
+    display2pm.html(savedEvent2pm);
+
+    let savedEvent3pm = localStorage.getItem("3pm");
+    let display3pm = $("#display3pm");
+    display3pm.html(savedEvent3pm);
+
+    let savedEvent4pm = localStorage.getItem("4pm");
+    let display4pm = $("#display4pm");
+    display4pm.html(savedEvent4pm);
+
+    let savedEvent5pm = localStorage.getItem("5pm");
+    let display5pm = $("#display5pm");
+    display5pm.html(savedEvent5pm);
+  }
+  updateDOM();
+  const saveBtns = $(".saveBtn");
+
+  for (let i = 0; i < saveBtns.length; i++) {
+    saveBtns[i].addEventListener("click", function (e) {
+      let id = e.target.id;
+      let parentElement = $(`#timeBlock${id}`);
+      let newValue = parentElement.children()[0].value;
+      localStorage.setItem(id, newValue);
+      //   console.log(localStorage);
+    });
+  }
+  //   console.log(saveBtns);
+});
